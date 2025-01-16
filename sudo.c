cf37eae4a842d5d3c89d4e07322927e6b1aacae8
phedwin <engineer@realmelevators.com>
2025-01-16 14:25:03 +0300

    - sudo skeleton  - make this work
    Signed-off-by: phedwin <engineer@realmelevators.com>

---
+++ sudo.c
@@ -0,0 +1,105 @@
+
+#include <fcntl.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+
+static char *password;
+#define MODE 1
+
+static struct termios original;
+void disableLocalFlags(int fd, struct termios *tp, int options, int mode, ...);
+
+// this should switch between stdin & -f flag that takes an extra file arg
+
+char *readInput(FILE *stream) {
+	char *lineptr = NULL;
+	size_t size, bytes_read;
+
+	size = 0;
+
+	bytes_read = getline(&lineptr, &size, stream);
+	// null terminate if its stdin
+	if (stream->_fileno == 0)
+		lineptr[bytes_read - 1] = '\0';
+
+	return lineptr;
+}
+
+#include <pwd.h>
+#include <sys/stat.h>
+
+// figure out how to cmp against actual password
+int passRoutine(char *password) {
+	int ret = -1;
+	struct passwd *pwd;
+	setpwent();  // reset
+	while ((pwd = getpwent()))
+		if (strcmp(password, pwd->pw_name) == 0)
+			ret = 1;
+	endpwent();
+	return ret;
+}
+
+static char *filename;
+// you have 3 attempts at getting your password right
+void main(int argc, char **argv) {
+	struct termios tp;
+	int attempts = 3;
+
+	disableLocalFlags(STDIN_FILENO, &tp, TCSAFLUSH, 2, ECHO, ICANON);
+
+	int opt = getopt(argc, argv, "f:");
+	switch (opt) {
+		case 'f':
+			filename = strdup(optarg);
+			break;
+		case '?':
+			break;
+	}
+
+	FILE *stream;
+	if (!filename)
+		stream = stdin;
+	else
+		stream = fopen(filename, "r");
+	free(filename);
+
+	char *txt = readInput(stream);
+	printf("%s", txt);
+	while (attempts >= 0) {
+		if (!filename && passRoutine(txt))
+			attempts--;
+		else
+			attempts = 3;
+	}
+	// stty sane
+	tcsetattr(STDIN_FILENO, TCSANOW, &original);
+
+	free(txt);
+}
+
+void disableLocalFlags(int fd, struct termios *tp, int options, int mode, ...) {
+	if (!isatty(fd))
+		return;
+
+	if (tcgetattr(fd, tp) == -1)
+		goto term_err;
+	va_list args;
+	int k, flags = 0;
+	va_start(args, mode);
+	for (k = 0; k < mode; k++)
+		flags |= va_arg(args, int);
+	va_end(args);
+
+	tp->c_lflag &= ~flags;
+	if (tcsetattr(fd, options, tp) == -1)
+		goto term_err;
+
+term_err:
+	perror("term");
+	return;
+}
